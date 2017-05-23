a6b0b6470a4c2bcf755ff175fbbeaff5c14daa81
Grace Stuart and Georgina Van Dort <georgina@>
2017-05-23 09:43:49 -0700

    add color, font, and background

--- app/src/main/java/com/epicodus/madlibsandriod/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibsandriod/MainActivity.java
@@ -1,13 +1,22 @@
 package com.epicodus.madlibsandriod;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.textView);
+        Typeface edgeracer = Typeface.createFromAsset(getAssets(), "fonts/edgeracer.ttf");
+
+        mAppNameTextView.setTypeface(edgeracer);
+
     }
 }
