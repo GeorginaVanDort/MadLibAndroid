b871edcb24ae942549d4e32bdd0d6f0c8750640c
Grace Stuart and Georgina Van Dort <georgina@>
2017-05-23 13:10:44 -0700

    add method to display array data

--- app/src/main/java/com/epicodus/madlibsandriod/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibsandriod/StoryActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.madlibsandriod;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class StoryActivity extends AppCompatActivity {
+
+    private TextView mHeroView;
+    private TextView mHopelessView;
+    private TextView mEmperorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        mEmperorView = (TextView) findViewById(R.id.emperorView);
+        mHopelessView = (TextView) findViewById(R.id.hopelessView);
+        mHeroView = (TextView) findViewById(R.id.heroView);
+
+        ArrayList<String> list = getIntent().getStringArrayListExtra("input");
+
+        mEmperorView.setText(list.get(0));
+        mHopelessView.setText(list.get(1));
+        mHeroView.setText(list.get(2));
+
     }
 }
--- app/src/main/java/com/epicodus/madlibsandriod/UserInput.java
+++ app/src/main/java/com/epicodus/madlibsandriod/UserInput.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class UserInput extends AppCompatActivity {
 
     private EditText mHero;
+    private EditText mHopeless;
+    private EditText mEmperor;
     private Button mGenerateButton;
     private ArrayList<String>mInputArrayList;
 
@@ -21,17 +23,27 @@ public class UserInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
 
+        mEmperor = (EditText) findViewById(R.id.emperor);
         mHero = (EditText) findViewById(R.id.hero);
+        mHopeless = (EditText) findViewById(R.id.hopeless);
         mGenerateButton = (Button) findViewById(R.id.generateButton);
 
         mGenerateButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 String hero = mHero.getText().toString();
+                String hopeless = mHopeless.getText().toString();
+                String emperor = mEmperor.getText().toString();
+
                 ArrayList<String>mInputArrayList = new ArrayList<String>();
                    mInputArrayList.add(hero);
+                   mInputArrayList.add(hopeless);
+                   mInputArrayList.add(emperor);
 
                 Intent intent = new Intent(UserInput.this, StoryActivity.class);
+                intent.putStringArrayListExtra("input", mInputArrayList);
+                startActivity(intent);
+
             }
         });
     }
